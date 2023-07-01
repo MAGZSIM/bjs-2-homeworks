c84a0f03b0df96f1c6a10a9001015d2d08a0165d
Magz <Magzsin@gmail.com>
2023-07-02 01:25:51 +0300

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,15 @@
-"use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
+"use strict";
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+function solveEquation(a, b, c) {
+   let discriminant = Math.pow(b,2) - 4*a*c;
+   if (discriminant < 0) {
+   return [];
+   } else if (discriminant === 0) {
+    return [-b/(2*a)];
+   } else if (discriminant > 0) {
+    return [(-b + Math.sqrt(discriminant) )/(2*a), (-b - Math.sqrt(discriminant) )/(2*a)]
+   }
 }
+console.log(solveEquation(1, 5, 4));
+console.log(solveEquation(1, 2, 1));
+console.log(solveEquation(1, 2, 10));
